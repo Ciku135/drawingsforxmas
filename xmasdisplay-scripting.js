ee9d5c36e998a38ccba0352fbc2845501a2ddd5b
Ciku135 <cikunjuguna09@gmail.com>
2025-12-23 23:04:51 +0300

    Added the pictures and their functions, added the footer and changed the height of the footer, etc. Will add more code soon

--- xmasdisplay-scripting.js
+++ xmasdisplay-scripting.js
@@ -6,5 +6,25 @@ function showtime() {
 setInterval(showtime, 1000);
 
 function surprise() {
-    window.location.href="https://www.youtube.com/watch?v=ALj9FPvyFUU";
+    window.location.href="https://www.youtube.com/watch?v=hJresi7z_YM";
+}
+
+function christmastree() {
+    alert("A Christmas Tree!\n~It is not what is under the Christmas Tree that matters,\nit's who's around it\n   -Charles M. Schulz");
+}
+
+function christmassweater() {
+    alert("A Christmas Sweater.\nMost people call it an ugly sweater.\nAny thoughts?");
+}
+
+function candycane() {
+    alert("It's a Candy Cane!\nThe edges look a bit off");
+}
+
+function christmasornament() {
+    alert("A Christmas Ornament.\nAs red as red can be, right?");
+}
+
+function christmassock() {
+    alert("A Christmas Sock to stuff presents and other socks in");
 }
